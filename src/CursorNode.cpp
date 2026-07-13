3c45124c91bbaea2dd0b14ff9057321a2737c1e5
Frostre_ <n9043395@gmail.com>
2026-07-13 16:03:39 +0200

    Replace sprite with a visible label for CursorNode

--- src/CursorNode.cpp
+++ src/CursorNode.cpp
@@ -18,36 +18,39 @@ bool CursorNode::init() {
 
     m_visible = true;
     m_cursorPos = CCDirector::sharedDirector()->getWinSize() / 2;
+    m_visual = nullptr;
 
-    // Create a simple 50x50 red square using a texture of a 1x1 pixel
-    auto texture = CCTextureCache::sharedTextureCache()->addImage("square.png");
-    if (!texture) {
-        // If the image doesn't exist, create a colored sprite from a 1x1 texture
-        // We'll just draw a colored sprite using a simple 1x1 texture created in code
-        // Simpler: use a CCLayerColor? Actually we can create a CCSprite with a custom texture.
-        // Easiest: use a CCRect with a solid color via CCSprite::createWithSpriteFrameName?
-        // Let's create a 1x1 pixel texture using CCTexture2D::create(int width, int height) - not available.
-        // Alternative: use CCLayerColor but it's a node that doesn't support positioning like a sprite.
-        // Fallback: use a CCLabelTTF with a filled block character: "█"
-        auto label = CCLabelTTF::create("█", "Arial", 50);
-        label->setColor(ccc3(255, 0, 0));
-        label->setPosition(ccp(0, 0)); // relative to this node
-        this->addChild(label);
-        m_sprite = label; // store as CCObject*
-    } else {
-        auto sprite = CCSprite::createWithTexture(texture);
-        sprite->setScale(50);
-        sprite->setColor(ccc3(255, 0, 0));
-        sprite->setPosition(ccp(0, 0));
-        this->addChild(sprite);
-        m_sprite = sprite;
+    // Create a big "X" label – very visible
+    auto label = CCLabelTTF::create("✚", "Arial", 60);
+    // Or use "+" if the character doesn't render
+    if (!label) {
+        label = CCLabelTTF::create("+", "Arial", 60);
     }
+    label->setColor(ccc3(255, 0, 0)); // bright red
+    label->setPosition(ccp(0, 0));
+    this->addChild(label);
+    m_visual = label;
 
     setPosition(m_cursorPos);
     CCNode::setVisible(true);
 
-    log::info("CursorNode initialized with sprite.");
+    log::info("CursorNode initialized with label.");
     return true;
+}
+
+void CursorNode::setVisible(bool visible) {
+    m_visible = visible;
+    CCNode::setVisible(visible);
+}
+
+void CursorNode::onEnter() {
+    CCNode::onEnter();
+    CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, -500, false);
+}
+
+void CursorNode::onExit() {
+    CCDirector::sharedDirector()->getTouchDispatcher()->removeDelegate(this);
+    CCNode::onExit();
 }
 
 void CursorNode::updatePosition(const CCPoint& pos) {
