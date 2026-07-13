0d8a4baa353221ac50216d90eb689e293bf5cffc
Frostre_ <n9043395@gmail.com>
2026-07-13 15:41:22 +0200

    Refactor CursorNode: streamline member variables and methods
    Removed unused member variables and methods, added update method for frame redraw.

--- src/CursorNode.hpp
+++ src/CursorNode.hpp
@@ -7,26 +7,19 @@ class CursorNode : public CCNode, public CCTouchDelegate {
 protected:
     bool m_visible;
     CCPoint m_cursorPos;
-    CCPoint m_prevTouchPos;   // for delta calculation
     float m_radius;
-    float m_scale;
-    float m_sensitivity;
-    ccColor4B m_color;
-    std::string m_shape;
+    CCDrawNode* m_drawNode;  // for drawing
 
 public:
     static CursorNode* create();
     bool init() override;
     void setVisible(bool visible) override;
-    void draw() override;
+    void update(float dt) override;  // redraw each frame
 
     void onEnter() override;
     void onExit() override;
 
-    void showAfterDelay(float delay);
-    void showMe(CCObject* sender);
-
-    void refreshSettings();   // safe, no callbacks
+    void updatePosition(const CCPoint& pos);
 
     bool ccTouchBegan(CCTouch* touch, CCEvent* event) override;
     void ccTouchMoved(CCTouch* touch, CCEvent* event) override;
