b3d7fdf1118473cc088ff5d7e878aa07b3982535
Frostre_ <n9043395@gmail.com>
2026-07-13 11:14:55 +0200

    Improve comments in CursorNode.cpp
    Refactor comments for clarity and consistency in CursorNode.

--- src/CursorNode.cpp
+++ src/CursorNode.cpp
@@ -32,8 +32,8 @@ void CursorNode::setVisible(bool visible) {
 void CursorNode::draw() {
     if (!m_visible) return;
 
-    // White color with a slight shadow effect (draw twice)
-    ccDrawColor4B(0, 0, 0, 100);  // Shadow
+    // Shadow (offset)
+    ccDrawColor4B(0, 0, 0, 100);
     glLineWidth(4.0f);
     ccDrawLine(
         ccp(m_cursorPos.x - m_radius + 2, m_cursorPos.y + 2),
@@ -45,21 +45,17 @@ void CursorNode::draw() {
     );
     ccDrawCircle(m_cursorPos + ccp(2, 2), m_radius, 0, 16, false);
 
-    // Main white drawing
+    // Main white
     ccDrawColor4B(255, 255, 255, 255);
     glLineWidth(2.0f);
-
-    // Horizontal line
     ccDrawLine(
         ccp(m_cursorPos.x - m_radius, m_cursorPos.y),
         ccp(m_cursorPos.x + m_radius, m_cursorPos.y)
     );
-    // Vertical line
     ccDrawLine(
         ccp(m_cursorPos.x, m_cursorPos.y - m_radius),
         ccp(m_cursorPos.x, m_cursorPos.y + m_radius)
     );
-    // Circle around
     ccDrawCircle(m_cursorPos, m_radius, 0, 16, false);
 }
 
@@ -68,15 +64,13 @@ void CursorNode::updatePosition(const CCPoint& pos) {
     setPosition(pos);
 }
 
-// Touch Delegate
 void CursorNode::registerWithTouchDispatcher() {
-    // Priority -500 means we get touches first, but `false` means we DON'T swallow them
+    // Priority -500, and `false` means we do NOT swallow touches
     CCDirector::get()->getTouchDispatcher()->addTargetedDelegate(this, -500, false);
 }
 
 bool CursorNode::ccTouchBegan(CCTouch* touch, CCEvent* event) {
-    // We always want to listen to moves/ends
-    return true;
+    return true; // We want to receive move/end events
 }
 
 void CursorNode::ccTouchMoved(CCTouch* touch, CCEvent* event) {
@@ -85,9 +79,9 @@ void CursorNode::ccTouchMoved(CCTouch* touch, CCEvent* event) {
 }
 
 void CursorNode::ccTouchEnded(CCTouch* touch, CCEvent* event) {
-    // Nothing needed here – game gets the click since we don't swallow
+    // Nothing – game gets the click because we don't swallow
 }
 
 void CursorNode::ccTouchCancelled(CCTouch* touch, CCEvent* event) {
-    // Nothing needed
+    // Nothing
 }
