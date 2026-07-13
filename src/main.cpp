e44f741254888963ae6476f1572420e0f0225fe7
Frostre_ <n9043395@gmail.com>
2026-07-13 16:30:26 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,34 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCScene.hpp>
-#include "CursorNode.hpp"
+#include <Geode/modify/AppDelegate.hpp>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 
 using namespace geode::prelude;
 
-CursorNode* g_cursor = nullptr;
+class $modify(MyAppDelegate, AppDelegate) {
+    bool applicationDidFinishLaunching() {
+        auto ret = AppDelegate::applicationDidFinishLaunching();
 
-class $modify(CursorSceneHook, CCScene) {
-    void onEnter() override {
-        CCScene::onEnter();
-        this->scheduleOnce(schedule_selector(CursorSceneHook::delayedManageCursor), 0.1f);
-    }
-
-    void onExit() override {
-        if (g_cursor && g_cursor->getParent() == this) {
-            g_cursor->retain();
-            g_cursor->removeFromParentAndCleanup(false);
-            g_cursor->release();
-            g_cursor = nullptr;
-        }
-        CCScene::onExit();
-    }
-
-    void delayedManageCursor(float dt) {
-        auto scene = this;
-        if (!scene) return;
-
-        // Debug label to confirm mod is loaded
-        auto label = CCLabelBMFont::create("Mousse Active!", "bigFont.fnt");
-        if (label) {
-            label->setPosition(CCDirector::sharedDirector()->getWinSize() / 2);
-            label->setPositionY(label->getPositionY() + 100);
-            label->setScale(0.8f);
-            label->setColor(ccc3(0, 255, 0));
-            label->setZOrder(10000);
-            scene->addChild(label);
+        // Write a timestamp to a file on external storage
+        std::ofstream file("/storage/emulated/0/mousse_test.txt");
+        if (file.is_open()) {
+            auto now = std::chrono::system_clock::now();
+            auto now_c = std::chrono::system_clock::to_time_t(now);
+            file << "Mousse mod loaded at: " << std::ctime(&now_c);
+            file.close();
         }
 
-        if (!g_cursor) {
-            g_cursor = CursorNode::create();
-            if (g_cursor) {
-                g_cursor->setZOrder(9999);
-                scene->addChild(g_cursor);
-                g_cursor->setVisible(true);
-                log::info("Cursor created (CCDrawNode).");
-            }
-        }
-        else if (g_cursor->getParent() != scene) {
-            g_cursor->retain();
-            g_cursor->removeFromParentAndCleanup(false);
-            scene->addChild(g_cursor);
-            g_cursor->release();
-            g_cursor->setVisible(true);
-            log::info("Cursor moved.");
-        }
+        log::info("Mousse mod loaded (file write test).");
+        return ret;
     }
 };
 
 $on_mod(Loaded) {
-    log::info("Mousse mod loaded (force show).");
+    // Also write from this hook (which runs earlier)
+    std::ofstream file("/storage/emulated/0/mousse_on_mod.txt");
+    if (file.is_open()) {
+        file << "on_mod hook executed!";
+        file.close();
+    }
 }
