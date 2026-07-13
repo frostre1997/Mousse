b53a29d5d5f7fde3de0166aab72150b42c933434
Frostre_ <n9043395@gmail.com>
2026-07-13 11:12:38 +0200

    Add cursor support to CCScene in main.cpp
    Implement cursor functionality in CCScene.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,51 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/AppDelegate.hpp>
+#include <Geode/modify/CCScene.hpp>
+#include "CursorNode.hpp"
 
 using namespace geode::prelude;
 
-class $modify(MyAppDelegate, AppDelegate) {
-    void trySaveApp(bool a) {
-        AppDelegate::trySaveApp(a);
-        log::info("Mouse on Android mod loaded successfully!");
+// Global pointer so we can access it anywhere
+CursorNode* g_cursor = nullptr;
+
+// Hook every scene to make sure the cursor is always present
+class $modify(CursorSceneHook, CCScene) {
+    bool init() {
+        if (!CCScene::init()) return false;
+        ensureCursor();
+        return true;
+    }
+
+    void onEnter() {
+        CCScene::onEnter();
+        ensureCursor();
+    }
+
+    void ensureCursor() {
+        if (!g_cursor) {
+            // Create cursor for the first time
+            g_cursor = CursorNode::create();
+            g_cursor->setZOrder(9999); // On top of everything
+            this->addChild(g_cursor);
+            g_cursor->setVisible(false); // Start hidden
+
+            // Show it after 1.5 seconds
+            Loader::get()->queueInMainThread([]() {
+                if (g_cursor) {
+                    g_cursor->setVisible(true);
+                }
+            }, 1.5f);
+        } 
+        else if (g_cursor->getParent() != this) {
+            // If cursor exists but is not in this scene, move it here
+            g_cursor->retain();
+            g_cursor->removeFromParentAndCleanup(false);
+            this->addChild(g_cursor);
+            g_cursor->release();
+        }
     }
 };
+
+// Optional: Log that the mod loaded successfully
+$on_mod(Loaded) {
+    log::info("Mousse mod loaded! Cursor will appear in 1.5s.");
+}
