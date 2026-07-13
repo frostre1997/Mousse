6a8af8b537c9931dfc6239953573224de0e177c7
Frostre_ <n9043395@gmail.com>
2026-07-13 22:27:27 +0200

    Refactor CursorNode class and update touch methods
    Refactor CursorNode class to use cocos2d namespace and remove unused members.

--- src/CursorNode.hpp
+++ src/CursorNode.hpp
@@ -1,24 +1,18 @@
 #pragma once
 #include <Geode/Geode.hpp>
 
-using namespace geode::prelude;
-
-class CursorNode : public CCNode, public CCTouchDelegate {
+class CursorNode : public cocos2d::CCNode {
 protected:
     bool m_visible;
-    float m_radius;
-
 public:
     static CursorNode* create();
     bool init() override;
     void setVisible(bool visible) override;
     void draw() override;
-
     void onEnter() override;
     void onExit() override;
-
-    bool ccTouchBegan(CCTouch* touch, CCEvent* event) override;
-    void ccTouchMoved(CCTouch* touch, CCEvent* event) override;
-    void ccTouchEnded(CCTouch* touch, CCEvent* event) override;
-    void ccTouchCancelled(CCTouch* touch, CCEvent* event) override;
+    bool ccTouchBegan(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
+    void ccTouchMoved(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
+    void ccTouchEnded(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
+    void ccTouchCancelled(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
 };
