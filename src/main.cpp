c8521f53f5286d3f1567679e863cf55e62447bb0
Frostre_ <n9043395@gmail.com>
2026-07-13 19:24:40 +0200

    Refactor cursor log messages for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,10 @@ CursorNode* g_cursor = nullptr;
 class $modify(CursorSceneHook, CCScene) {
     void onEnter() override {
         CCScene::onEnter();
-        // Schedule after a tiny delay to ensure the scene is fully set up
         this->scheduleOnce(schedule_selector(CursorSceneHook::delayedAddCursor), 0.0f);
     }
 
     void onExit() override {
-        // When the scene is about to be destroyed, detach and release the cursor
         if (g_cursor && g_cursor->getParent() == this) {
             g_cursor->retain();
             g_cursor->removeFromParentAndCleanup(false);
@@ -28,28 +26,26 @@ class $modify(CursorSceneHook, CCScene) {
         auto scene = this;
         if (!scene) return;
 
-        // If cursor doesn't exist, create it
         if (!g_cursor) {
             g_cursor = CursorNode::create();
             if (g_cursor) {
                 g_cursor->setZOrder(99999);
                 scene->addChild(g_cursor);
                 g_cursor->setVisible(true);
-                log::info("Cursor created and added to scene.");
+                log::info("Cursor created.");
             }
         }
-        // If cursor exists but belongs to another scene, move it here
         else if (g_cursor->getParent() != scene) {
             g_cursor->retain();
             g_cursor->removeFromParentAndCleanup(false);
             scene->addChild(g_cursor);
             g_cursor->release();
             g_cursor->setVisible(true);
-            log::info("Cursor moved to new scene.");
+            log::info("Cursor moved.");
         }
     }
 };
 
 $on_mod(Loaded) {
-    log::info("Mousse mod loaded (scene hook).");
+    log::info("Mousse mod loaded.");
 }
