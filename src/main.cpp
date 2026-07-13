0b86f8f0f462bc38debec0b4735328275370dccc
Frostre_ <n9043395@gmail.com>
2026-07-13 19:22:51 +0200

    Modify cursor management to use CCScene
    Refactor cursor handling to work with CCScene instead of MenuLayer. Implement onEnter and onExit methods for proper cursor management.

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,55 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCScene.hpp>
 #include "CursorNode.hpp"
 
 using namespace geode::prelude;
 
 CursorNode* g_cursor = nullptr;
 
-class $modify(MyMenuLayer, MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
+class $modify(CursorSceneHook, CCScene) {
+    void onEnter() override {
+        CCScene::onEnter();
+        // Schedule after a tiny delay to ensure the scene is fully set up
+        this->scheduleOnce(schedule_selector(CursorSceneHook::delayedAddCursor), 0.0f);
+    }
 
-        // Remove the test red layer – we no longer need it
+    void onExit() override {
+        // When the scene is about to be destroyed, detach and release the cursor
+        if (g_cursor && g_cursor->getParent() == this) {
+            g_cursor->retain();
+            g_cursor->removeFromParentAndCleanup(false);
+            g_cursor->release();
+            g_cursor = nullptr;
+        }
+        CCScene::onExit();
+    }
 
-        // Create and add the cursor
+    void delayedAddCursor(float dt) {
+        auto scene = this;
+        if (!scene) return;
+
+        // If cursor doesn't exist, create it
         if (!g_cursor) {
             g_cursor = CursorNode::create();
             if (g_cursor) {
                 g_cursor->setZOrder(99999);
-                this->addChild(g_cursor);
-                log::info("Cursor created on MenuLayer.");
+                scene->addChild(g_cursor);
+                g_cursor->setVisible(true);
+                log::info("Cursor created and added to scene.");
             }
         }
-        else if (g_cursor->getParent() != this) {
+        // If cursor exists but belongs to another scene, move it here
+        else if (g_cursor->getParent() != scene) {
             g_cursor->retain();
             g_cursor->removeFromParentAndCleanup(false);
-            this->addChild(g_cursor);
+            scene->addChild(g_cursor);
             g_cursor->release();
+            g_cursor->setVisible(true);
             log::info("Cursor moved to new scene.");
         }
-
-        return true;
     }
 };
 
 $on_mod(Loaded) {
-    log::info("Mousse mod loaded (production version).");
+    log::info("Mousse mod loaded (scene hook).");
 }
