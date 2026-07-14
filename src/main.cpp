84ee4420e0e5c9b2e622f5dee042b113c50e0972
Frostre_ <n9043395@gmail.com>
2026-07-14 10:10:42 +0200

    Refactor cursor handling in MyMenuLayer
    Refactor cursor creation logic to remove old cursor before creating a new one. Ensure cursor visibility when MenuLayer is shown again.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include "CursorNode.hpp"
 
 using namespace geode::prelude;
 
+// Global cursor pointer
 CursorNode* g_cursor = nullptr;
 
 class $modify(MyMenuLayer, MenuLayer) {
@@ -12,21 +12,36 @@ class $modify(MyMenuLayer, MenuLayer) {
 
         auto winSize = CCDirector::get()->getWinSize();
 
-        // Create and center the cursor
-        if (!g_cursor) {
-            g_cursor = CursorNode::create();
-            if (g_cursor) {
-                g_cursor->setZOrder(1000);  // High Z-order so it's on top
-                this->addChild(g_cursor);
-                g_cursor->setPosition(ccp(winSize.width / 2, winSize.height / 2));
-                g_cursor->setVisible(true);
-                log::info("[Mousse] Cursor created at position: {}x{}", winSize.width/2, winSize.height/2);
-            } else {
-                log::error("[Mousse] Failed to create cursor!");
-            }
+        // Remove old cursor if it exists (from previous MenuLayer instance)
+        if (g_cursor) {
+            g_cursor->removeFromParentAndCleanup(true);
+            g_cursor = nullptr;
+        }
+
+        // Create NEW cursor
+        g_cursor = CursorNode::create();
+        if (g_cursor) {
+            g_cursor->setZOrder(1000);  // High Z-order so it's on top
+            g_cursor->setPosition(ccp(winSize.width / 2, winSize.height / 2));
+            g_cursor->setVisible(true);
+            this->addChild(g_cursor);
+            
+            log::info("[Mousse] Cursor created at: {}x{}", winSize.width/2, winSize.height/2);
         }
 
         return true;
+    }
+    
+    // Add this to handle when layer is shown again
+    void onEnter() override {
+        MenuLayer::onEnter();
+        log::info("[Mousse] MenuLayer onEnter called");
+        
+        // Make sure cursor is visible when returning
+        if (g_cursor) {
+            g_cursor->setVisible(true);
+            g_cursor->setTouchEnabled(true);
+        }
     }
 };
 
