2e5a133273545e43b8eb9b8b0959e29b75b65661
Frostre_ <n9043395@gmail.com>
2026-07-14 11:19:53 +0200

    Refactor cursor management in MyMenuLayer
    Changed global cursor pointer to static and adjusted Z-order for visibility. Removed unnecessary logging and touch handling code.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 
 using namespace geode::prelude;
 
-// Global cursor pointer
-CursorNode* g_cursor = nullptr;
+static CursorNode* g_cursor = nullptr;
 
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() override {
@@ -13,64 +12,47 @@ class $modify(MyMenuLayer, MenuLayer) {
 
         auto winSize = CCDirector::get()->getWinSize();
 
-        // Remove old cursor if it exists
         if (g_cursor) {
             g_cursor->removeFromParentAndCleanup(true);
             g_cursor = nullptr;
         }
 
-        // Create NEW cursor
         g_cursor = CursorNode::create();
         if (g_cursor) {
-            g_cursor->setZOrder(1000);
             g_cursor->setPosition(ccp(winSize.width / 2, winSize.height / 2));
-            g_cursor->setVisible(true);
+            g_cursor->setZOrder(99999);
             this->addChild(g_cursor);
-            
-            log::info("[Mousse] Cursor created at: {}x{}", winSize.width/2, winSize.height/2);
         }
 
-        // Enable touch on this layer so we can track touches
-        this->setTouchEnabled(true);
-        this->setTouchPriority(-1000);
-        
         return true;
     }
-    
-    // Track touches on MenuLayer and move the cursor
+
     bool ccTouchBegan(CCTouch* touch, CCEvent* event) override {
         if (g_cursor) {
             auto pos = touch->getLocation();
             g_cursor->setPosition(pos);
-            log::info("[Mousse] Touch began - moving cursor to: {}x{}", pos.x, pos.y);
         }
         return MenuLayer::ccTouchBegan(touch, event);
     }
-    
+
     void ccTouchMoved(CCTouch* touch, CCEvent* event) override {
         if (g_cursor) {
             auto pos = touch->getLocation();
             g_cursor->setPosition(pos);
-            log::info("[Mousse] Touch moved - cursor at: {}x{}", pos.x, pos.y);
         }
         MenuLayer::ccTouchMoved(touch, event);
     }
-    
+
     void ccTouchEnded(CCTouch* touch, CCEvent* event) override {
-        log::info("[Mousse] Touch ended");
         MenuLayer::ccTouchEnded(touch, event);
     }
-    
+
     void ccTouchCancelled(CCTouch* touch, CCEvent* event) override {
-        log::info("[Mousse] Touch cancelled");
         MenuLayer::ccTouchCancelled(touch, event);
     }
-    
-    // Handle returning from online
+
     void onEnter() override {
         MenuLayer::onEnter();
-        log::info("[Mousse] MenuLayer onEnter called");
-        
         if (g_cursor) {
             g_cursor->setVisible(true);
         }
@@ -78,5 +60,5 @@ class $modify(MyMenuLayer, MenuLayer) {
 };
 
 $on_mod(Loaded) {
-    log::info("[Mousse] Mousse mod loaded successfully!");
+    log::info("[Mousse] loaded");
 }
